b8a3160f2aa444ac401abf98bfa54b94af819e2d
Neger008 <alfredo.full.cf@gmail.com>
2019-04-20 11:33:01 -0500

    Modifique la clase persona

--- src/main/java/com/fulcito/consulta/model/Person.java
+++ src/main/java/com/fulcito/consulta/model/Person.java
@@ -12,6 +12,7 @@ package com.fulcito.consulta.model;
 public class Person {
     private Integer id;
     private String name;
+    private String fatherName;
 
     public Integer getId() {
         return id;
@@ -21,6 +22,14 @@ public class Person {
         this.id = id;
     }
 
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+ 
     public String getName() {
         return name;
     }
